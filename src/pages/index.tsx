690321f0ca61ff898fbdeafeee1d4a5235008a19
apipkurniawan <davitaja92@gmail.com>
2025-08-20 08:09:56 +0700

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import Layout from '@/components/layout/layout';
-import Seo from '@/components/seo';
+import Layout from "@/components/layout";
+import Seo from "@/components/seo";
 
 function index() {
   return (
     <Layout>
-      <Seo templateTitle='web view' siteName='Ocean' />
+      <Seo templateTitle="web view" siteName="Ocean" />
 
       <div>this is web view components</div>
     </Layout>
